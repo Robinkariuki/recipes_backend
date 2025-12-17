3c9104c64252d229018185a860ae96ae7b9fa0c3
Robin Kariuki <robinkaris7@gmail.com>
2025-12-17 13:17:47 +0300

    fixed issue with accessing env variables

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
+export const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URI as string || "mongodb:// localhost:27017/recipes_db");  
+        const conn = await mongoose.connect(process.env.MONGO_URI as string || "mongodb://127.0.0.1:27017/recipe");  
         console.log(`MongoDB Connected: ${conn.connection.host}`); 
     } catch (error) {
         console.error(`Error: ${(error as Error).message}`);
         process.exit(1);
     }
 };
+
+
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
+import "dotenv/config";
 import app from './app.js';
+import { connectDB } from "./config/db.js";
+
+
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    message: "API is running",
+    timestamp: new Date().toISOString()
+  });
 });
+
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
